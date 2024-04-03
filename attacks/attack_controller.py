2b22245080146c8bfef5cddc7f54891705bcbc50
Adi <23616271+maladi17@users.noreply.github.com>
2024-04-03 23:33:57 +0300

    Update attack_controller.py

--- attacks/attack_controller.py
+++ attacks/attack_controller.py
@@ -3,6 +3,7 @@ from attacks.handlers.base_handler import Request, Response
 from utils.configuration import Configuration
 from attacks.handlers.application_rw_handler import ApplicationRW_Handler
 from attacks.handlers.user_rw_handler import UserRW_Handler
+from attacks.handlers.au_handler import AU_Handler
 from attacks.handlers import *
 
 from utils import logger
@@ -23,11 +24,14 @@ class AttackController:
         
         conf = Configuration().get_config()
         request = Request(self.token,conf,self.tenantId,self.appId,self.roles)
+        
         application_rw_vector_attack = ApplicationRW_Handler()
         user_rw_vector_attack = UserRW_Handler()
         user_invite_handler = UserInvite_Handler()
+        au_rw_handler = AU_Handler()
 
         application_rw_vector_attack.set_next(user_rw_vector_attack)
         user_rw_vector_attack.set_next(user_invite_handler)
+        user_invite_handler.set_next(au_rw_handler)
         
         return application_rw_vector_attack.handle(request,[])
