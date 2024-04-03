61d67d5e7257f781b6cb3168409ee80f3069f1cf
Adi <23616271+maladi17@users.noreply.github.com>
2024-04-03 23:36:52 +0300

    Update application_rw_handler.py

--- attacks/handlers/application_rw_handler.py
+++ attacks/handlers/application_rw_handler.py
@@ -10,7 +10,7 @@ class ApplicationRW_Handler(AttackHandler):
 
     def handle(self, request: Request,responses:List[Response]):     
         # TODO map request.roles to fit with this attack
-        if True:
+        if "Application.ReadWrite.All" in request.roles:
             status = False
             attack_name = "ApplicationRWVectors"
             message = ""
